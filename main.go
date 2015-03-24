310b61696d623746fc3bf296b718af941ddc67c1
NHOrus <jy6x2b32pie9@yahoo.com>
2015-03-24 22:58:38 +0300

    Added boilerplate for termui.
    Now, need to fill it with info from proc for pleasurable digestion

--- main.go
+++ main.go
@@ -2,12 +2,39 @@
 package main
 
 import (
-	"fmt"
-	proc "github.com/cespare/goproc"
+	//	proc "github.com/cespare/goproc"
 	ui "github.com/gizak/termui"
 	tm "github.com/nsf/termbox-go"
+	// "time"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	err := ui.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer ui.Close()
+
+	//getting ready to close stuff on command
+	evt := make(chan tm.Event)
+	go func() {
+		for {
+			evt <- tm.PollEvent()
+		}
+	}()
+
+	for {
+		select {
+		case e := <-evt:
+			if e.Type == tm.EventKey && e.Ch == 'q' {
+				return
+			}
+			if e.Type == tm.EventResize {
+				ui.Body.Width = ui.TermWidth()
+				ui.Body.Align()
+			}
+		default:
+			//	draw(i)
+		}
+	}
 }
