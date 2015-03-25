bd55d608452414cf75982f0505e50d9531995917
NHOrus <jy6x2b32pie9@yahoo.com>
2015-03-25 07:45:11 +0300

    Corrected types, added only function to be here
    copy of actions for swap

--- memory.go
+++ memory.go
@@ -2,14 +2,30 @@
 package main
 
 import (
-//	proc "github.com/cespare/goproc"
+	proc "github.com/cespare/goproc"
 )
 
 type memData struct {
-	memTotal    int64
-	memFree     int64
+	memTotal    uint64
+	memFree     uint64
 	memPercent  float64
-	swapTotal   int64
-	swapFree    int64
+	swapTotal   uint64
+	swapFree    uint64
 	swapPercent float64
+}
+
+func (m *memData) Update() error {
+	t, err := proc.MemInfo()
+	if err != nil {
+		return err
+	}
+	m.memTotal = t["MemTotal"]
+	m.memFree = t["MemFree"]
+	m.memPercent = float64((m.memTotal - m.memFree) / m.memTotal)
+
+	m.swapTotal = t["SwapTotal"]
+	m.swapFree = t["SwapFree"]
+	m.swapPercent = float64((m.swapTotal - m.swapFree) / m.swapTotal)
+
+	return nil
 }
