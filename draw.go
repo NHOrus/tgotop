f8322c3a4aecb93a1f7383c1a857cd85112e80ba
NHOrus <jy6x2b32pie9@yahoo.com>
2015-11-04 08:08:30 +0300

    Visible output, termui.v2 is buggy very much

--- draw.go
+++ draw.go
@@ -5,18 +5,18 @@ package main
 import (
 	"bytes"
 	"fmt"
-	//	spew "github.com/davecgh/go-spew/spew"
+
+	"github.com/davecgh/go-spew/spew"
+
 	ui "gopkg.in/gizak/termui.v2"
 	//tm "github.com/nsf/termbox-go"
-	"os"
-	"os/signal"
-	"syscall"
 	"text/tabwriter"
 	"time"
 )
 
 const (
 	mult  = 10
+	rtick = time.Second / 60
 	atick = time.Second  //averaging interval
 	ptick = atick / mult //polling interval
 )
@@ -40,8 +40,8 @@ func main() {
 	gNet := ui.NewList()
 
 	//getting ready to close stuff on command
-	sig := make(chan os.Signal)
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGABRT, syscall.SIGTERM)
+	//	sig := make(chan os.Signal)
+	//	signal.Notify(sig, os.Interrupt, os.Kill)
 
 	nd := new(netData)
 	err = nd.Init(3*mult, ptick)
@@ -60,13 +60,16 @@ func main() {
 		ui.NewRow(ui.NewCol(12, 0, qMess)))
 
 	ui.Body.Align()
+	ui.Render(ui.Body)
 
-	ui.Handle("/timer/0.016s", func(ui.Event) {
+	ui.Merge("timer/update", ui.NewTimerCh(ptick))
+	ui.Merge("timer/render", ui.NewTimerCh(rtick))
+
+	ui.Handle("/timer/"+ptick.String(), func(ui.Event) {
 		err := m.Update()
 		if err != nil {
 			panic(err)
 		}
-
 		gMem.Percent = m.memPercent
 		gMem.BorderLabel = fillfmt("Memory", m.memUse, m.memTotal)
 
@@ -74,6 +77,12 @@ func main() {
 		gSwap.BorderLabel = fillfmt("Swap", m.swapUse, m.swapTotal)
 
 		gNet.Items = netf(nd)
+
+		ui.Render(ui.Body)
+	})
+
+	ui.Handle("/timer/"+rtick.String(), func(ui.Event) {
+		ui.Render(ui.Body)
 	})
 
 	ui.Handle("/sys/kdb/q", func(ui.Event) {
@@ -89,9 +98,14 @@ func main() {
 	})
 
 	ui.Handle("/sys/wnd/resize", func(ui.Event) {
-		ui.Body.Width = ui.TermWidth()
-		ui.Body.Align()
+		if ui.TermWidth() > 20 {
+			ui.Body.Width = ui.TermWidth()
+			ui.Body.Align()
+		}
+		ui.Render(ui.Body)
 	})
+
+	spew.Dump(ui.DefaultEvtStream)
 
 	ui.Loop()
 }
